b9affd01d5303070daba80160a70d078fa6432ba
Mame Daba DIOUF <devmobile@alima-ngo.local>
2018-08-06 19:10:43 +0100

    TCHANDRA V1.3

--- tchandra/models/tchandra_suivi.py
+++ tchandra/models/tchandra_suivi.py
@@ -15,9 +15,9 @@ from odoo.exceptions import UserError
 _logger = logging.getLogger(__name__)
 
 VACCINS = [
-    ('oui','Oui'),
-    ('non','Non'),
-    ('ne_sait_pas','Ne sait pas')
+    ('1','Ne sait pas'),
+    ('2','Non'),
+    ('3','Oui')
 ]
 
 STATUT_VITAL_FEMME = [
@@ -44,15 +44,48 @@ class Suivi(models.Model):
     date_deces=fields.Date(string="Date du décès")
     statut_vital_enfant=fields.Selection(STATUT_VITAL_ENFANT, string="Statut vital de l'enfant")
     date_deces_enfant=fields.Date(string="Date du décès de l'enfant")
-    poids_enfant=fields.Float(string="Poids de l'enfant")
+    poids_enfant=fields.Char(string="Poids de l'enfant")
     poids_non_realise=fields.Boolean(string="Mesure non réalisée")
-    taille_enfant=fields.Integer(string="Taille de l'enfant")
+    taille_enfant=fields.Char(string="Taille de l'enfant")
     taille_non_realise=fields.Boolean(string="Mesure non réalisée")
-    perimetre_cranien=fields.Integer(string="Périmètre crânien")
+    perimetre_cranien=fields.Char(string="Périmètre crânien")
     perimetre_cranien_non_realise=fields.Boolean(string="Mesure non réalisée")
     bcg=fields.Selection(VACCINS, string="BCG")
     polio=fields.Selection(VACCINS, string="Polio 0")
     hepatite_b=fields.Selection(VACCINS, string="Hépatite B")
+    statut_vital_enfant_deux=fields.Selection(STATUT_VITAL_ENFANT, string="Statut vital du deuxième enfant")
+    date_deces_enfant_deux=fields.Date(string="Date du décès du deuxième enfant")
+    poids_enfant_deux=fields.Char(string="Poids du deuxième enfant")
+    poids_non_realise_deux=fields.Boolean(string="Mesure non réalisée")
+    taille_enfant_deux=fields.Char(string="Taille du deuxième enfant")
+    taille_non_realise_deux=fields.Boolean(string="Mesure non réalisée")
+    perimetre_cranien_deux=fields.Char(string="Périmètre crânien du deuxième enfant")
+    perimetre_cranien_non_realise_deux=fields.Boolean(string="Mesure non réalisée")
+    bcg_deux=fields.Selection(VACCINS, string="BCG")
+    polio_deux=fields.Selection(VACCINS, string="Polio 0")
+    hepatite_b_deux=fields.Selection(VACCINS, string="Hépatite B")
+    statut_vital_enfant_trois=fields.Selection(STATUT_VITAL_ENFANT, string="Statut vital du troisième enfant")
+    date_deces_enfant_trois=fields.Date(string="Date du décès du troisème enfant")
+    poids_enfant_trois=fields.Char(string="Poids du troisième enfant")
+    poids_non_realise_trois=fields.Boolean(string="Mesure non réalisée")
+    taille_enfant_trois=fields.Char(string="Taille du troisième enfant")
+    taille_non_realise_trois=fields.Boolean(string="Mesure non réalisée")
+    perimetre_cranien_trois=fields.Char(string="Périmètre crânien du troisième enfant")
+    perimetre_cranien_non_realise_trois=fields.Boolean(string="Mesure non réalisée")
+    bcg_trois=fields.Selection(VACCINS, string="BCG")
+    polio_trois=fields.Selection(VACCINS, string="Polio 0")
+    hepatite_b_trois=fields.Selection(VACCINS, string="Hépatite B")
+    statut_vital_enfant_quatre=fields.Selection(STATUT_VITAL_ENFANT, string="Statut vital du quatrième enfant")
+    date_deces_enfant_quatre=fields.Date(string="Date du décès du quatrième enfant")
+    poids_enfant_quatre=fields.Char(string="Poids du quatrième enfant")
+    poids_non_realise_quatre=fields.Boolean(string="Mesure non réalisée")
+    taille_enfant_quatre=fields.Char(string="Taille du quatrième enfant")
+    taille_non_realise_quatre=fields.Boolean(string="Mesure non réalisée")
+    perimetre_cranien_quatre=fields.Char(string="Périmètre crânien du quatrième enfant")
+    perimetre_cranien_non_realise_quatre=fields.Boolean(string="Mesure non réalisée")
+    bcg_quatre=fields.Selection(VACCINS, string="BCG")
+    polio_quatre=fields.Selection(VACCINS, string="Polio 0")
+    hepatite_b_quatre=fields.Selection(VACCINS, string="Hépatite B")
     grossesse = fields.One2many('tchandra.grossesse', 'suivi', string='Femmes enceintes')
     @api.one
     def incremente_id(self, vals_id):
